87a4a4cd9984563282fcbead337c3514f2dd5399
Zakharchenko Svetlana <zakharchenko.svetlanka@bk.ru>
2022-06-13 18:33:21 +0500

    add adapter

--- app/src/main/java/com/example/shopitem/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shopitem/presentation/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         viewModel.shopList.observe(this){
             Log.d("MainActivity", it.toString())
             if(count == 0){
-                count += 1
+                count++
                 val item = it[0]
                 viewModel.deleteShopItem(item)
             }
